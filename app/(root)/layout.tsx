ee6c83e9eba9a3dac44e457fb35073ca0bf1d8ca
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2024-06-14 13:17:50 +0200

    Initialized mobile nav and right sidebar components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -10,9 +13,24 @@ export default function RootLayout({
         <main className="relative flex bg-black-3">
             <LeftSidebar/>
 
-            
-            {children}
-            <p className="text-white-1">RIGHT SIDEBAR</p>
+            <section className="flex flex-col min-h-screen flex-1 px-4 sm:px-14 border-2 border-red-500">
+              <div className="mx-auto flex flex-col w-full max-w-5xl max-sm:px-4">
+                <div className="flex h-16 items-center justify-between md:hidden">
+                  <Image 
+                    src='/icons/logo.svg' 
+                    width={30} 
+                    height={30}
+                    alt='menu icon'
+                    />
+                  <MobileNav />
+                </div>
+                <div>
+                  Toaster
+                  {children}
+                </div>
+              </div>
+            </section>
+            <RightSidebar/>
         </main>
     </div>
   );
