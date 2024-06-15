09f56e35d9bb30216f62d84aa9f84836c51478bb
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2024-06-15 14:25:32 +0200

    Added Convex BaaS configuration files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,25 @@
+"use client";
 import PodcastCard from '@/components/PodcastCard'
 import { podcastData } from '@/constants'
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
-const page = () => {
+
+
+
+
+const Home = () => {
+
+  const tasks = useQuery(api.tasks.get);
+
   return (
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending podcasts</h1>
+
+        <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+        </div>
 
         <div className='podcast_grid'>
           {podcastData.map(({ id, imgURL, title, description}) => (
@@ -23,4 +37,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Home
