77f8a9ce7da87812a3f2573da5e88bd3c345c1c6
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2024-06-17 10:59:27 +0200

    added route protection with clerk

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up', '/'])
+
+export default clerkMiddleware((auth, req) => {
+    if(!isPublicRoute(req)) {
+        auth().protect();
+    }
+})
+
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
