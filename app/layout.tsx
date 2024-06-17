e0ab27ad4fe84a39011ab3ee6e120e8bd72dcfe8
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2024-06-17 10:51:15 +0200

    Added Clerk auth provider with Convex configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ConvexClientProvider from "./providers/ConvexProvider";
+import ConvexClerkProvider from "./providers/ConvexClerkProvider";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,9 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexClientProvider>
+        <ConvexClerkProvider>
           {children}
-        </ConvexClientProvider>
+        </ConvexClerkProvider>
       </body>
     </html>
   );
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://loyal-jay-97.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
