fb6ab4516eff93f390ee408c5452be11af954fec
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2024-06-17 21:52:13 +0200

    Added initial Clerk auth pages styling

--- app/providers/ConvexClerkProvider.tsx
+++ app/providers/ConvexClerkProvider.tsx
@@ -9,7 +9,19 @@ import { ReactNode } from "react";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 const ConvexClerkProvider = ({ children }: { children: ReactNode} ) => (
-  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string} appearance={{
+    layout: { 
+      socialButtonsVariant: 'iconButton',
+      logoImageUrl: '/icons/logo.svg'
+    },
+    variables: {
+      colorBackground: '#15171c',
+      colorPrimary: '',
+      colorText: 'white',
+      colorInputBackground: '#1b1f29',
+      colorInputText: 'white'
+    }
+  }}>
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
       {children}
     </ConvexProviderWithClerk>
