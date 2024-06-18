d656b9ba0161fa34a1aee98d555581fb55e65326
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2024-06-18 21:46:18 +0200

    changes toast styling

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border bg-primaryPink-1 text-white-1",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
